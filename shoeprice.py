89f4bb2afe4a62eaf37b15232366788c084de819
nlist2 <noah2list@gmail.com>
2020-03-29 12:48:21 -0500

    error resolution

--- shoeprice.py
+++ shoeprice.py
@@ -28,28 +28,37 @@ driver = webdriver.Firefox(options=options)
 print(colored("Driver successfully created as Firefox headless...", "green"))
 
 # Stock X prices:
-print("Getting StockX.com...")
-driver.get("https://www.stockx.com")
+try:
+    print("Getting StockX.com...")
+    driver.get("https://www.stockx.com")
 
-#Finding the search input and sending keys of the shoe that we want
-driver.find_element_by_id("home-search").send_keys(str(shoe) + Keys.RETURN)
-print("Getting " + str(shoe) + "...")
+    #Finding the search input and sending keys of the shoe that we want
+    driver.find_element_by_id("home-search").send_keys(str(shoe) + Keys.RETURN)
+    print("Getting " + str(shoe) + "...")
 
-#First element in result and clicking it
-shoe_tile = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "tile.Tile-c8u7wn-0.bCufAv")))
-shoe_tile.click()
+    #First element in result and clicking it
+    shoe_tile = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "tile.Tile-c8u7wn-0.bCufAv")))
+    shoe_tile.click()
 
-#Size number
-#implicitywait
-size_box = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "fa.fa-chevron-down")))
-size_box.click()
+    #Size number
+    #implicitywait
+    size_box = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "fa.fa-chevron-down")))
+    size_box.click()
 
-#Picking your size...
-size_div = driver.find_element_by_class_name("list-unstyled.sneakers")
-size_div.find_elements_by_class_name("title")
+    #Picking your size...
+    size_div = driver.find_element_by_class_name("list-unstyled.sneakers")
+    size_div.find_elements_by_class_name("title")
 
-for x in range(len(size_div)):
-    if(size_div[x].text == str(size)):
-        stockx_price = size_div[x].find_element_by_class_name("subtitle").text
-    
-print(colored("StockX price: " + str(stockx_price), "green"))
+    for x in range(len(size_div)):
+        if(size_div[x].text == str(size)):
+            stockx_price = size_div[x].find_element_by_class_name("subtitle").text
+        
+    print(colored("StockX price: " + str(stockx_price), "green"))
+
+except:
+    print(colored("StockX failed.... Continuing...", "red"))
+
+try:
+    driver.get("https://www.ebay.com")
+except:
+    print(colored("Ebay failed.... Continuing...", "red"))
